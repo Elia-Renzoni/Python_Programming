b4a20c16ee5b9b45d3e2d18c2936fe24416cb916
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-04-06 15:20:24 +0200

    Create lists.py

---
+++ basics/lists.py
@@ -0,0 +1,13 @@
+# Introduction
+# @author Elia Renzoni
+# @brief List in Python
+
+list_of_same_types = [12, 3, 4, 5, 6]
+print(list_of_same_types)
+print(list_of_same_types[2:])
+print(list_of_same_types[-4])   # print 3
+
+list_of_different_types = ["str", 12, 5.5]
+print(list_of_different_types)
+list_of_different_types.append("OSS")
+print(list_of_different_types)
