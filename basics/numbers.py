584fd8bb221532dcc6954f06303039a722f119af
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-04-06 14:57:21 +0200

    Create numbers.py

---
+++ basics/numbers.py
@@ -0,0 +1,33 @@
+# Introduction
+# @author Elia Renzoni
+# @brief variables and calculus
+
+
+# basic sum
+first_value = 10
+second_value = 22
+
+print(first_value + second_value)
+
+# mul and div
+mul1 = 10
+mul2 = 22
+result = 10 * 22
+print(result)
+
+div1 = 10
+div2 = 22.5
+result_div = div1 / div2
+print(result_div)
+
+# exp
+first = 10
+second = 2
+exp_result = 10 ** 2
+print(exp_result)
+
+# module
+print(10 % 5)
+
+# floor division
+print(17 // 3)
