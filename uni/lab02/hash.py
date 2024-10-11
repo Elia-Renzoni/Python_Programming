f9458233710e9b0722ba7dc124e6cd7e682f8a33
unknown <elia.renzoni03@gmail.com>
2024-10-11 12:56:24 +0200

    .

--- uni/lab02/hash.py
+++ uni/lab02/hash.py
@@ -11,5 +11,11 @@ def addInMap(hashMap, key, value):
     hashMap[key] = value
     return True
 
+def delteInMap(hashMap, key):
+    if hashMap.has_key(key):
+        return False
+    del hashMap[key]
+    return True
+
 print(addInMap(myMap, 12, "Pippo"))
 print(addInMap(myMap, 40, "Pluto"))
