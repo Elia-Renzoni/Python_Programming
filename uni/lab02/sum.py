89be80c9aff2a9419a2c319ab197c2ca534e343b
Elia <elia.renzoni03@gmail.com>
2024-10-25 12:24:31 +0200

    add numpy ex

--- uni/lab02/sum.py
+++ uni/lab02/sum.py
@@ -12,4 +12,5 @@ print(simpleSum(12, 3))
 def keywordsArgFunc(par1=0, par2=0, par3=0):
     return par1 * par2 * par3
 
+
 print(keywordsArgFunc(par1=2, par2=2, par3=1))
---
+++ uni/lab04/numpy_demo.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a)
+print(a[1])
+print(type(a))
+print(type(a[1]))
+print(a.dtype)
+
+# matrix 3x3
+a2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a2)
+# ndim, n-dimensionalità dell'array, mono e bi.
+print(a.ndim)
+print(a2.ndim)
+# numero di elementi in lunghezza e larghezza
+# (3)
+print(a.shape)
+# (3, 3)
+print(a2.shape)
+
+# size
+print("Size: quanti elementi sono contenuti nell'array per dimensione:")
+print(a.size)
+print(a2.size)
+
+print(a2[1])
+# accesso al primo elemento della dimensione in posizione 1
+print(a2[1][0])
